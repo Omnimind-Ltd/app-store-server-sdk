496a247f217d170b5b60c8a798094a6c9c32ea0f
chirag729 <chirag@omnimind.tech>
2021-11-09 13:53:03 +0000

    add example

---
+++ example/main.dart
@@ -0,0 +1,22 @@
+import 'package:app_store_server_sdk/app_store_server_sdk.dart';
+
+void main() async {
+  String? token; // load existing token if available
+
+  var appStoreEnvironment = AppStoreEnvironment.sandbox(
+      bundleId: 'app_bundle_id',
+      issuerId: 'issuer_id',
+      keyId: 'private_key_id',
+      privateKey: 'private_key');
+
+  var appStoreHttpClient = AppStoreServerHttpClient(appStoreEnvironment,
+      jwt: token, jwtTokenUpdatedCallback: (token) async {
+    // Persist token for later re-use
+  });
+
+  var api = AppStoreServerAPI(appStoreHttpClient);
+
+  var statusResponse = await api.getAllSubscriptionStatuses('1000000907113638');
+  var historyResponse = await api.getTransactionHistory('1000000907113638');
+  var refundLookupResponse = await api.getRefundHistory('1000000907113638');
+}
