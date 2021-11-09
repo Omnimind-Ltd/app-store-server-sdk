720d8dac47767c340181254faa328b1f6502f5d4
chirag729 <chirag@omnimind.tech>
2021-11-09 15:51:00 +0000

    added JWSTransactionDecodedPayload and JWSRenewalInfoDecodedPayload

--- lib/src/model/status_response.dart
+++ lib/src/model/status_response.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:jose/jose.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'status_response.g.dart';
@@ -70,6 +73,14 @@ class LastTransactionsItem {
   /// The transaction information signed by Apple, in JWS format.
   final String signedTransactionInfo;
 
+  /// The decoded renewal information
+  JWSRenewalInfoDecodedPayload get renewalInfo =>
+      JWSRenewalInfoDecodedPayload.fromEncodedPayload(signedRenewalInfo);
+
+  /// The decoded transaction information
+  JWSTransactionDecodedPayload get transactionInfo =>
+      JWSTransactionDecodedPayload.fromEncodedPayload(signedTransactionInfo);
+
   const LastTransactionsItem(this.originalTransactionId, this.status,
       this.signedRenewalInfo, this.signedTransactionInfo);
 
@@ -83,5 +94,179 @@ class LastTransactionsItem {
     return 'LastTransactionsItem{originalTransactionId: $originalTransactionId,'
         ' status: $status, signedRenewalInfo: $signedRenewalInfo,'
         ' signedTransactionInfo: $signedTransactionInfo}';
+  }
+}
+
+/// A decoded payload containing subscription renewal information.
+@JsonSerializable()
+class JWSRenewalInfoDecodedPayload {
+  /// The identifier of the product that renews at the next billing period.
+  final String autoRenewProductId;
+
+  /// The renewal status of the auto-renewable subscription.
+  final int autoRenewStatus;
+
+  /// The reason the subscription expired.
+  final int expirationIntent;
+
+  /// The time when the grace period for subscription renewals expires.
+  final String? gracePeriodExpiresDate;
+
+  /// The Boolean value that indicates whether the App Store is attempting to automatically renew an expired subscription.
+  final bool isInBillingRetryPeriod;
+
+  /// The promo code or the promotional offer identifier.
+  final String? offerIdentifier;
+
+  /// The type of the promotional offer.
+  final int? offerType;
+
+  /// The transaction identifier of the original purchase associated with this transaction.
+  final String originalTransactionId;
+
+  /// The status indicating whether a customer has approved a subscription price increase.
+  final int? priceIncreaseStatus;
+
+  /// The unique identifier of the product.
+  final String productId;
+
+  /// The UNIX time, in milliseconds, that the App Store signed the JSON Web Signature data.
+  final int signedDate;
+
+  const JWSRenewalInfoDecodedPayload(
+      this.autoRenewProductId,
+      this.autoRenewStatus,
+      this.expirationIntent,
+      this.gracePeriodExpiresDate,
+      this.isInBillingRetryPeriod,
+      this.offerIdentifier,
+      this.offerType,
+      this.originalTransactionId,
+      this.priceIncreaseStatus,
+      this.productId,
+      this.signedDate);
+
+  factory JWSRenewalInfoDecodedPayload.fromEncodedPayload(String payload) {
+    var jws = JsonWebSignature.fromCompactSerialization(payload);
+    return JWSRenewalInfoDecodedPayload.fromJson(
+        jsonDecode(jws.unverifiedPayload.stringContent));
+  }
+
+  Map<String, dynamic> toJson() => _$JWSRenewalInfoDecodedPayloadToJson(this);
+
+  factory JWSRenewalInfoDecodedPayload.fromJson(Map<String, dynamic> json) =>
+      _$JWSRenewalInfoDecodedPayloadFromJson(json);
+
+  @override
+  String toString() {
+    return 'JWSRenewalInfoDecodedPayload{autoRenewProductId: $autoRenewProductId, '
+        'autoRenewStatus: $autoRenewStatus, expirationIntent: $expirationIntent, '
+        'gracePeriodExpiresDate: $gracePeriodExpiresDate, '
+        'isInBillingRetryPeriod: $isInBillingRetryPeriod, '
+        'offerIdentifier: $offerIdentifier, offerType: $offerType, '
+        'originalTransactionId: $originalTransactionId, '
+        'priceIncreaseStatus: $priceIncreaseStatus, productId: $productId, '
+        'signedDate: $signedDate}';
+  }
+}
+
+/// A decoded payload containing transaction information.
+@JsonSerializable()
+class JWSTransactionDecodedPayload {
+  /// he UUID you created to identify the user’s in-app account when they made the purchase. If your app doesn’t provide an appAccountToken, this string is empty.
+  final String? appAccountToken;
+
+  /// The bundle identifier of the app.
+  final String bundleId;
+
+  /// The UNIX time, in milliseconds, the subscription expires or renews.
+  final int expiresDate;
+
+  /// A string that describes whether the transaction was purchased by the user, or is available to them through Family Sharing.
+  final String inAppOwnershipType;
+
+  /// The Boolean value that indicates whether the user upgraded to another subscription.
+  final bool? isUpgraded;
+
+  /// The identifier that contains the promo code or the promotional offer identifier.
+  final String? offerIdentifier;
+
+  /// A value that represents the promotional offer type.
+  final String? offerType;
+
+  /// The UNIX time, in milliseconds, that represents the purchase date of the original transaction identifier.
+  final int originalPurchaseDate;
+
+  /// The transaction identifier of the original purchase.
+  final String originalTransactionId;
+
+  /// The unique identifier of the product.
+  final String productId;
+
+  /// The UNIX time, in milliseconds, that the App Store charged the user’s account for a purchase, restored product, subscription, or subscription renewal after a lapse.
+  final int purchaseDate;
+
+  /// The number of consumable products the user purchased.
+  final int quantity;
+
+  /// The UNIX time, in milliseconds, that Apple Support refunded a transaction.
+  final int? revocationDate;
+
+  /// The UNIX time, in milliseconds, that the App Store signed the JSON Web Signature data.
+  final int signedDate;
+
+  /// The identifier of the subscription group the subscription belongs to.
+  final String subscriptionGroupIdentifier;
+
+  /// The unique identifier of the transaction.
+  final String transactionId;
+
+  /// The type of the in-app purchase.
+  final String type;
+
+  /// A unique ID that identifies subscription purchase events across devices, including subscription renewals.
+  final String webOrderLineItemId;
+
+  JWSTransactionDecodedPayload(
+      this.appAccountToken,
+      this.bundleId,
+      this.expiresDate,
+      this.inAppOwnershipType,
+      this.isUpgraded,
+      this.offerIdentifier,
+      this.offerType,
+      this.originalPurchaseDate,
+      this.originalTransactionId,
+      this.productId,
+      this.purchaseDate,
+      this.quantity,
+      this.revocationDate,
+      this.signedDate,
+      this.subscriptionGroupIdentifier,
+      this.transactionId,
+      this.type,
+      this.webOrderLineItemId);
+
+  factory JWSTransactionDecodedPayload.fromEncodedPayload(String payload) {
+    var jws = JsonWebSignature.fromCompactSerialization(payload);
+    return JWSTransactionDecodedPayload.fromJson(
+        jsonDecode(jws.unverifiedPayload.stringContent));
+  }
+
+  Map<String, dynamic> toJson() => _$JWSTransactionDecodedPayloadToJson(this);
+
+  factory JWSTransactionDecodedPayload.fromJson(Map<String, dynamic> json) =>
+      _$JWSTransactionDecodedPayloadFromJson(json);
+
+  @override
+  String toString() {
+    return 'JWSTransactionDecodedPayload{appAccountToken: $appAccountToken, bundleId: $bundleId, '
+        'expiresDate: $expiresDate, inAppOwnershipType: $inAppOwnershipType, '
+        'isUpgraded: $isUpgraded, offerIdentifier: $offerIdentifier, '
+        'offerType: $offerType, originalPurchaseDate: $originalPurchaseDate, '
+        'originalTransactionId: $originalTransactionId, productId: $productId, '
+        'purchaseDate: $purchaseDate, quantity: $quantity, revocationDate: $revocationDate, '
+        'signedDate: $signedDate, subscriptionGroupIdentifier: $subscriptionGroupIdentifier, '
+        'transactionId: $transactionId, type: $type, webOrderLineItemId: $webOrderLineItemId}';
   }
 }
--- lib/src/model/status_response.g.dart
+++ lib/src/model/status_response.g.dart
@@ -67,3 +67,98 @@ Map<String, dynamic> _$LastTransactionsItemToJson(
       'signedRenewalInfo': instance.signedRenewalInfo,
       'signedTransactionInfo': instance.signedTransactionInfo,
     };
+
+JWSRenewalInfoDecodedPayload _$JWSRenewalInfoDecodedPayloadFromJson(
+        Map<String, dynamic> json) =>
+    JWSRenewalInfoDecodedPayload(
+      json['autoRenewProductId'] as String,
+      json['autoRenewStatus'] as int,
+      json['expirationIntent'] as int,
+      json['gracePeriodExpiresDate'] as String?,
+      json['isInBillingRetryPeriod'] as bool,
+      json['offerIdentifier'] as String?,
+      json['offerType'] as int?,
+      json['originalTransactionId'] as String,
+      json['priceIncreaseStatus'] as int?,
+      json['productId'] as String,
+      json['signedDate'] as int,
+    );
+
+Map<String, dynamic> _$JWSRenewalInfoDecodedPayloadToJson(
+    JWSRenewalInfoDecodedPayload instance) {
+  final val = <String, dynamic>{
+    'autoRenewProductId': instance.autoRenewProductId,
+    'autoRenewStatus': instance.autoRenewStatus,
+    'expirationIntent': instance.expirationIntent,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('gracePeriodExpiresDate', instance.gracePeriodExpiresDate);
+  val['isInBillingRetryPeriod'] = instance.isInBillingRetryPeriod;
+  writeNotNull('offerIdentifier', instance.offerIdentifier);
+  writeNotNull('offerType', instance.offerType);
+  val['originalTransactionId'] = instance.originalTransactionId;
+  writeNotNull('priceIncreaseStatus', instance.priceIncreaseStatus);
+  val['productId'] = instance.productId;
+  val['signedDate'] = instance.signedDate;
+  return val;
+}
+
+JWSTransactionDecodedPayload _$JWSTransactionDecodedPayloadFromJson(
+        Map<String, dynamic> json) =>
+    JWSTransactionDecodedPayload(
+      json['appAccountToken'] as String?,
+      json['bundleId'] as String,
+      json['expiresDate'] as int,
+      json['inAppOwnershipType'] as String,
+      json['isUpgraded'] as bool?,
+      json['offerIdentifier'] as String?,
+      json['offerType'] as String?,
+      json['originalPurchaseDate'] as int,
+      json['originalTransactionId'] as String,
+      json['productId'] as String,
+      json['purchaseDate'] as int,
+      json['quantity'] as int,
+      json['revocationDate'] as int?,
+      json['signedDate'] as int,
+      json['subscriptionGroupIdentifier'] as String,
+      json['transactionId'] as String,
+      json['type'] as String,
+      json['webOrderLineItemId'] as String,
+    );
+
+Map<String, dynamic> _$JWSTransactionDecodedPayloadToJson(
+    JWSTransactionDecodedPayload instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('appAccountToken', instance.appAccountToken);
+  val['bundleId'] = instance.bundleId;
+  val['expiresDate'] = instance.expiresDate;
+  val['inAppOwnershipType'] = instance.inAppOwnershipType;
+  writeNotNull('isUpgraded', instance.isUpgraded);
+  writeNotNull('offerIdentifier', instance.offerIdentifier);
+  writeNotNull('offerType', instance.offerType);
+  val['originalPurchaseDate'] = instance.originalPurchaseDate;
+  val['originalTransactionId'] = instance.originalTransactionId;
+  val['productId'] = instance.productId;
+  val['purchaseDate'] = instance.purchaseDate;
+  val['quantity'] = instance.quantity;
+  writeNotNull('revocationDate', instance.revocationDate);
+  val['signedDate'] = instance.signedDate;
+  val['subscriptionGroupIdentifier'] = instance.subscriptionGroupIdentifier;
+  val['transactionId'] = instance.transactionId;
+  val['type'] = instance.type;
+  val['webOrderLineItemId'] = instance.webOrderLineItemId;
+  return val;
+}
