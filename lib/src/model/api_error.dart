27bba047ad2086988b3ec992d28b2a073023556e
chirag729 <chirag@omnimind.tech>
2021-11-10 10:59:40 +0000

    add errorCode consts

--- lib/src/model/api_error.dart
+++ lib/src/model/api_error.dart
@@ -2,6 +2,27 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'api_error.g.dart';
 
+//Retryable errors
+const accountNotFoundRetryableError = 4040002;
+const appNotFoundRetryableError = 4040004;
+const generalInternalRetryableError = 5000001;
+const originalTransactionIdNotFoundRetryableError = 4040006;
+
+// Errors
+const accountNotFoundError = 4040001;
+const appNotFoundError = 4040003;
+const generalInternalError = 5000000;
+const generalBadRequestError = 4000000;
+const invalidAppIdentifierError = 4000002;
+const invalidExtendByDaysError = 4000009;
+const invalidExtendReasonCodeError = 4000010;
+const invalidOriginalTransactionIdError = 4000008;
+const invalidRequestIdentifierError = 4000011;
+const invalidRequestRevisionError = 4000005;
+const originalTransactionIdNotFoundError = 4040005;
+const subscriptionExtensionIneligibleError = 4030004;
+const subscriptionMaxExtensionError = 4030005;
+
 /// Errors returned by App Store Server API responses.
 @JsonSerializable()
 class ApiError {
